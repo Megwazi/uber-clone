82d9763649a0d7fdeab3de1c1e4fc2fe6d904abf
Megwazi <rabello.lucas@outlook.com>
2024-05-23 23:26:18 -0300

    tentativa de usar API maps

--- components/home/InputItem.js
+++ components/home/InputItem.js
@@ -1,15 +1,23 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import Image from 'next/image'
+import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
 
-function InputItem({type}) {
-  return (
-    <div className='bg-slate-200 p-3 rounded-lg mt-3
-    flex items-center gap-4'>
-        <Image src={type =='source'?'/src.svg':'/dest.svg'} width={20} height={20}/>
-        <input type='text' placeholder={type=='source'?'Local de partida':'Destino final'}
-        className='bg-transparent w-full outline-none'/>
-    </div>
-  )
+function InputItem({ type }) {
+    const [value, setValue] = useState(null);
+    return (
+        <div className='bg-slate-200 p-3 rounded-lg mt-3 flex items-center gap-4'>
+            <Image src={type == 'source' ? '/src.svg' : '/dest.svg'} width={20} height={20} />
+
+            <GooglePlacesAutocomplete apiKey={process.env.NEXT_PUBLIC_GOOGLE_API_KEY}
+                selectProps={{
+                    value,
+                    onChange: setValue,
+                }}
+            />
+
+        </div>
+    )
 }
 
 export default InputItem
