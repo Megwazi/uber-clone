f637615eebe2876c666934870077a9ff9be55892
Megwazi <rabello.lucas@outlook.com>
2024-07-08 20:40:58 -0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import MapsEng from "@/components/home/MapsEng";
 import SearchEng from "@/components/home/SearchEng";
 import { DestinationContext } from "@/context/DestinationContext";
 import { SourceContext } from "@/context/SourceContext";
+import { LoadScript } from "@react-google-maps/api";
 import { useState } from "react";
 
 export default function Home() {
@@ -12,6 +13,9 @@ export default function Home() {
   return (
     <SourceContext.Provider value={{ source, setSource }}>
       <DestinationContext.Provider value={{ destination, setDestination }}>
+        <LoadScript 
+        libraries={['places']}
+        googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}>
         <div className="p-6 grid grid-cols-1 md:grid-cols-3 gap-5">
           <div>
             <SearchEng />
@@ -20,6 +24,7 @@ export default function Home() {
             <MapsEng />
           </div>
         </div>
+        </LoadScript>
       </DestinationContext.Provider>
     </SourceContext.Provider>
   );
